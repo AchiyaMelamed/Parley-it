3b1fdc80f2bf54ff552d8a1df5dac2867d7457e9
AchiyaMelamed <aciya100@gmail.com>
2022-09-12 02:45:08 +0300

    little change in celery

--- api/celery.py
+++ api/celery.py
@@ -14,8 +14,13 @@ def pay_once_aweek(transactions_list):
     today = date.today()
     for transaction in transactions_list:
         if transaction.schedule == today:
+            # TODO: check how to run the post requests
             # perform_transaction(transaction)
-            done.append(transaction)
+            # res = download_report()
+            # if res[transaction.id] == "fail":
+            #     transaction.schedule = transactions_list[-2].schedule
+            # else:
+                done.append(transaction)
     for t in done:
         transactions_list.pop(transactions_list.index(t))
     sleep(5)
